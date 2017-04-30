837a6f7fe3f51796f69c5d5096f50c41089d3a6a
ameybarapatre <ameybarapatre@gmail.com>
2017-04-30 12:55:26 +0530

    Adding Rules  in Rules.txt Generating Output File

--- run.py
+++ run.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 output =  pd.read_csv('dataset.csv')
-data = pd.read_csv("fuzzyq.csv")
+data = pd.read_csv("fuzzyq1.csv")
 taxis = data[["ua","ub","uc","ud"]].as_matrix()
-taxis_y = data[["ub","ud"]].as_matrix()
+taxis_y = data[["ub","ud" , "ua"]].as_matrix()
 E = anfis.taxi_eligibility(taxis ,taxis_y=taxis_y ,alpha= 0.1 ,Gamma = 0.7)
 print(E.max())
 data['Q'] = E.ravel()
